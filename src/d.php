ac87dd8dcf7801a5a90e9c257bb18298bcebbc37
Szymon <sd@phase-ii.com>
2016-09-07 13:06:21 +0100

    first commit

---
+++ src/d.php
@@ -0,0 +1,27 @@
+<?php
+
+function d($data, $return = false, $trace = null) {
+
+    if (!function_exists('dump')) {
+        throw new \Exception("Can't find dump() function, install: composer global require symfony/var-dumper");
+    }
+
+    if (!$trace) {
+        $trace = debug_backtrace();
+    }
+
+    $trace = array_shift($trace);
+
+    $line = $trace['file'].':'.$trace['line'];
+
+    if ('cli' === PHP_SAPI) {
+        $line .= "\n";
+    }
+    else {
+        $line = '<div style="padding:3px;background-color:#B13030;color:#F9FF00;font:12px Menlo,Monaco,Consolas,monospace;margin-bottom:-12px;">'.$line.'</div>';
+    }
+
+    echo $line;
+
+    dump($data);
+}
